6c7fb97deed72db13c6e847af4aae8a89b347e20
quentin.detre@pm.me <quentin.detre4254@gmail.com>
2025-11-26 16:34:42 +0100

    Added pagination

--- www/public/app.php
+++ www/public/app.php
@@ -9,14 +9,42 @@ use Twig\Error\SyntaxError;
 $twig = getTwig();
 $manager = getMongoDbManager();
 
-$list =  $manager->selectCollection('tp')->find([])->toArray();
+// Configuration
+$limit = 10;
+$page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+$collection = $manager->selectCollection('tp');
+
+// Compter le total
+$totalDocuments = $collection->countDocuments([]);
+$maxPages = ceil($totalDocuments / $limit);
+
+// Sécurité : Gestion des dépassements de page
+// Si on demande la page 0 ou moins, on force la 1
+if ($page < 1) {
+    $page = 1;
+}
+// Si on demande une page supérieure au max (ex: 192 sur 42), on force la dernière page
+if ($page > $maxPages && $maxPages > 0) {
+    $page = $maxPages;
+}
+
+// Calcul du skip
+$skip = ($page - 1) * $limit;
+
+// Récupération des données
+$list = $collection->find([], [
+    'limit' => $limit,
+    'skip'  => $skip,
+    'sort'  => ['titre' => 1]
+])->toArray();
 
 // render template
 try {
-    echo $twig->render('index.html.twig', ['list' => $list]);
+    echo $twig->render('index.html.twig', [
+        'list'     => $list,
+        'page'     => $page,
+        'maxPages' => $maxPages
+    ]);
 } catch (LoaderError|RuntimeError|SyntaxError $e) {
     echo $e->getMessage();
 }
-
-
-
